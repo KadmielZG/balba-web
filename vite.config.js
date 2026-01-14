c6df355522920e0be684622fde158dbe3806eea0
Kevin <kczg98@gmail.com>
2026-01-14 17:30:12 -0600

    GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: "https://KadmielZG.github.io/balba-web", // 👈 NOMBRE EXACTO DEL REPO
+   base: "/balba-web/", // 👈 NOMBRE EXACTO DEL REPO
   plugins: [react(),tailwindcss()],
 })
