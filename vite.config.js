e134f513afb47c96267dceb8b8d381dfc2e681c4
Kevin <kczg98@gmail.com>
2026-01-14 17:08:55 -0600

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: '/balba-web/', // 👈 NOMBRE EXACTO DEL REPO
   plugins: [react(),tailwindcss()],
 })
